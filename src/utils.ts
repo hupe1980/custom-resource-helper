e4acbb48efd68dd472086774c776d5e83b40953a
hupe1980 <frankhuebner1980@gmail.com>
2020-08-04 13:42:32 +0200

    feat: Typesafe camelizeKeys

--- src/utils.ts
+++ src/utils.ts
@@ -1,21 +1,3 @@
-import camelCase from 'lodash.camelcase';
+import camelcaseKeys from 'camelcase-keys';
 
-const isObject = (obj: unknown):obj is Record<string, unknown> => obj !== null && typeof obj === 'object';
-
-const isArray = (arr: unknown):arr is Array<string | Record<string, unknown>> => Array.isArray(arr);
-
-export const camelizeKeys = (obj: unknown): unknown => {
-    if (isArray(obj)) {
-      return obj.map(v => camelizeKeys(v));
-    } else if (isObject(obj)) {
-      return Object.keys(obj).reduce(
-        (acc, key) => ({
-          ...acc,
-          [camelCase(key)]: camelizeKeys(obj[key])
-        }),
-        {}
-      );
-    }
-
-    return obj;
-}
+export const camelizeKeys = <T>(obj: T): T => camelcaseKeys<T>(obj, { deep: true });
